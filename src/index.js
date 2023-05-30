9f8589500d032036d4265e6aa1440fa7914f4f34
jennri <121411741+jennri@users.noreply.github.com>
2023-05-30 13:18:04 +1000

    adding items to list, renders properly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
 alert('hello')
 
 const listContainer = document.querySelector("[data-list]")
-let list = [];
+const newListForm = document.querySelector("[data-new-list]")
+const newListInput = document.querySelector("[data-new-input]")
+
+//
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'wash laundry'
+}];
+
+newListForm.addEventListener('submit', e => {
+    //prevents submission, as doing so will refresh the page
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName === "") return
+    
+    const list = createList(listName)
+    newListInput.value = null;
+    lists.push(list)
+    render();
+})
+
+function createList(name) {
+    return {id: Date.now().toString(), name: name, task: []}
+}
+
+//clears the list and 
+function render(){
+    clearElement(listContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement("li")
+        listElement.dataset.listId = list.id;
+        listElement.classList.add("listname")
+        listElement.innerText = list.name
+        listContainer.appendChild(listElement)
+    })
+
+}
+
+function clearElement(ele){
+    while (ele.firstChild) {
+        ele.removeChild(ele.firstChild)
+    }
+}
+
+render();
